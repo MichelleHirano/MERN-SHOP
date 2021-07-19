2ca5f0d31d5873db9fdb2394ff11003e724f7f52
MHirano <michelle@hiranohomes.com>
2021-07-18 18:48:53 -0700

    edit package json in client

---
+++ client/build/precache-manifest.5f1d04570dc375c20d8bd47a05c430e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c0e90b5805c6cfee9f3ec4e7e8adf8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "396a06afd05ee7806aae",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "71c76f8e2642c91e05de",
+    "url": "/static/js/2.a5d01bba.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.a5d01bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "396a06afd05ee7806aae",
+    "url": "/static/js/main.d7b8f76b.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
